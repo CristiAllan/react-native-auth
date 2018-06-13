32115740e3afca8c1eca78ef320556efb5c8b57b
CristiAllan <cristiallan@gmail.com>
2018-06-13 17:06:37 -0300

    add firebase to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAMKtMYY5g2o12xCf0Fanl9UveGnBPcEl8',
+      authDomain: 'react-native-auth-2c49c.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-2c49c.firebaseio.com',
+      projectId: 'react-native-auth-2c49c',
+      storageBucket: 'react-native-auth-2c49c.appspot.com',
+      messagingSenderId: '307532514969'
+    });
+  }
+
   render() {
     return (
       <View>
