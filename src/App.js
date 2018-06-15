e4dcaa68a0416aa6d8049d54b8c8a69d9a76073a
CristiAllan <cristiallan@gmail.com>
2018-06-15 17:34:59 -0300

    set loggedIn state to app.js and verification if user is loggedIn or not

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
+import firebase from 'firebase'
 import { Header } from './components/common';
 
 class App extends Component {
+
+  state = { loggedIn: false }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAMKtMYY5g2o12xCf0Fanl9UveGnBPcEl8',
@@ -13,6 +17,14 @@ class App extends Component {
       projectId: 'react-native-auth-2c49c',
       storageBucket: 'react-native-auth-2c49c.appspot.com',
       messagingSenderId: '307532514969'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
 
