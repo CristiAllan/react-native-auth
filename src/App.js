2cf5ff64d9cfde91ae519ad8cc617cf7320b27a7
CristiAllan <cristiallan@gmail.com>
2018-06-29 19:19:38 -0300

    config app view to different logedIn cases

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { View } from 'react-native';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 import firebase from 'firebase'
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 
 class App extends Component {
 
-  state = { loggedIn: false }
+  state = { loggedIn: null }
 
   componentWillMount() {
     firebase.initializeApp({
@@ -28,11 +28,22 @@ class App extends Component {
     });
   }
 
+  renderContent() {
+    switch(this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
